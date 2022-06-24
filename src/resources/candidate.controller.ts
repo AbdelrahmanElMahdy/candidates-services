e737964e7affd69b57b017219934daaa63f198de
mahdy <a.b.mahdey@gmail.com>
2022-06-24 22:05:55 +0200

    adding validation middleware to create request

--- src/resources/candidate.controller.ts
+++ src/resources/candidate.controller.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import Controller from '../utils/interfaces/controller.interface';
 import HttpException from '../utils/exceptions/http.exception';
+import validationMiddleware from '../middleware/validation.middleware';
+import validate from './candidate.validation';
 
 class CandidateController implements Controller {
     public path = '/candidates';
@@ -11,9 +13,13 @@ class CandidateController implements Controller {
     }
 
     private initializeRoutes(): void {
-        this.router.post(`${this.path}`, this.createCandidates);
-        this.router.get(`${this.path}/:id(\\d)?`, this.getCandidates)
+        this.router.post(
+            `${this.path}`,
+            validationMiddleware(validate.createCandidate),
+            this.createCandidates
+        );
 
+        this.router.get(`${this.path}/:id(\\d)?`, this.getCandidates);
     }
     private createCandidates = async (
         request: Request,
@@ -21,21 +27,21 @@ class CandidateController implements Controller {
         next: NextFunction
     ): Promise<Response | void> => {
         try {
-            response.status(201).json('candidate created');
+            response.status(201).json(request.body);
         } catch (error) {
             next(new HttpException(400, 'Cannot create candidate'));
         }
     };
-    
+
     private getCandidates = async (
         request: Request,
         response: Response,
         next: NextFunction
     ): Promise<Response | void> => {
         try {
-            response.status(200).json({"candidates_id":[1,2,3,4,4]});
+            response.status(200).json({ candidates_id: [1, 2, 3, 4, 4] });
         } catch (error) {
-            next(new HttpException(400, 'Cannot create candidate'));
+            next(new HttpException(400, 'Cannot retrieve candidate'));
         }
     };
 }
