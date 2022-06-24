e1b83b2473e0821ec064057efa7ad02ea25338b2
mahdy <a.b.mahdey@gmail.com>
2022-06-24 21:57:17 +0200

    adding candidates validation
    adding candidates Joi validation for creating new candidates

---
+++ src/resources/candidate.validation.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+const createCandidate = Joi.object({
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    job_title: Joi.string().required(),
+    phone: Joi.string().required(),
+    avatar: Joi.string().uri(),
+});
+
+export default { createCandidate };
