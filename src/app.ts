5e430a383cc3464de7b342a2170ff378b89758c2
mahdy <a.b.mahdey@gmail.com>
2022-06-24 21:06:31 +0200

    initialize the app

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express, { Application } from "express";
+import cors from "cors";
+import Controller from "./utils/interfaces/controller.interfaces";
+import compression from "compression";
+import morgan from "morgan";
+import helmet from "helmet";
+
+class App {
+  public express: Application;
+  public port: number = 5000;
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express();
+    this.port = port;
+
+    this.initializeMiddleware();
+    this.initializeControllers(controllers);
+  }
+  private initializeMiddleware(): void {
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(morgan("dev"));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+
+  private initializeControllers(controllers: Controller[]): void {
+    controllers.forEach((controller: Controller) => {
+      this.express.use("/v1", controller.router);
+    });
+  }
+
+  public listen(): void {
+    this.express.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+}
+export default App;
